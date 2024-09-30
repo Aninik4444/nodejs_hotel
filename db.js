e423ccedbf6eaab5f05d387d02572acecf595961
Aninik4444 <nikitakumarisnd2000@gmail.com>
2024-09-30 19:45:52 +0530

    changes to mongodb to online server

--- db.js
+++ db.js
@@ -6,8 +6,7 @@ require('dotenv').config();
 const mongoURL = process.env.MONGODB_URL;
 // set up mongodb connection
 mongoose.connect(mongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    useNewUrlParser: true
 })
 //  get the default connection
 //  Mongoose maintain a default connection object representing the mongodb connection
